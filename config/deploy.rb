53df52950a9cff148df406268588977cd13396b5
Dan Sadaka <dan@web-site1.com>
2017-01-16 20:37:41 -0500

    15 mins: now link sqlite db

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :format, :pretty
 set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml db/staging.sqlite3}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # set :rvm_ruby_string, :local
 set :rvm_ruby_version, '2.3.0@dymo --create'      # Defaults to: 'default'
